c80055c919925e22e0b396a32d809841e3d73ce2
Kawachi <kawachi.shusaku@opensesame-tech.com>
2019-03-20 20:11:28 +0900

    マウスでの移動完

---
+++ Guidance/Assets/Scripts/Services/Scenes/ISceneService.cs
@@ -0,0 +1,29 @@
+﻿namespace Guidance.Services.Scenes {
+
+	/// <summary>
+	/// シーン操作Serviceのインタフェース
+	/// </summary>
+	public interface ISceneService {
+
+		/// <summary>
+		/// シングルモードで遷移した場合の前シーンの名前取得
+		/// </summary>
+		/// <returns>シーン名</returns>
+		string GetBeforeSingleModeSceneName();
+
+		/// <summary>
+		/// シングルモードで遷移した場合の現在のシーン名取得
+		/// </summary>
+		/// <returns>シーン名</returns>
+		string GetCurrentSingleModeSceneName();
+
+		/// <summary>
+		/// シーン読み込み
+		/// </summary>
+		/// <param name="sceneName">読み込みシーン名</param>
+		/// <param name="parameter">遷移先シーンに渡すパラメータ</param>
+		void LoadScene( string sceneName , object parameter );
+
+	}
+
+}
